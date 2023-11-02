3d475309ffc378e19eed75948c72b7c0a5351e98
1chooo <hugo970217@gmail.com>
2023-11-02 14:07:14 +0800

    update: hw03

--- weather_analysis/23_fall/hw03/main.py
+++ weather_analysis/23_fall/hw03/main.py
@@ -11,10 +11,11 @@ import matplotlib.pyplot as plt
 import cartopy.feature as cfeature
 import os
 
-def load_data(filename):
+def load_data(filename: str):
     data = np.fromfile(filename, dtype='<f4')
     var, nlev, nlat, mlon = 3, 3, 46, 91
     data = data.reshape(var, nlev, nlat, mlon)
+
     return data
 
 def configure_parameters(data):
@@ -24,16 +25,21 @@ def configure_parameters(data):
     h = data[0, :, :, :]
     u = data[1, :, :, :]
     v = data[2, :, :, :]
+
     return lon, lat, h, u, v
 
-def geowind(h, lat, lon):
+def count_geowind(h, lat, lon):
     nlev, nlat, mlon = h.shape
     nlat = 46
     mlon = 91
     omega = 7.29 * (10 ** -5)
     dy = 6378000 * np.pi / 180
-    geo_windU = np.zeros([nlev, nlat, mlon])
-    geo_windV = np.zeros([nlev, nlat, mlon])
+    geo_windU = np.zeros(
+        [nlev, nlat, mlon]
+    )
+    geo_windV = np.zeros(
+        [nlev, nlat, mlon]
+    )
 
     for i in range(nlev):
         for j in range(nlat):
@@ -42,36 +48,36 @@ def geowind(h, lat, lon):
                 dx = dy * np.cos(lat[j] * np.pi / 180)
 
                 if 1 <= j < nlat - 1 and 1 <= k < mlon - 1:
-                    xvalue = -(h[i, j + 1, k] - h[i, j - 1, k]) / (2 * dy * f)
-                    yvalue = (h[i, j, k + 1] - h[i, j, k - 1]) / (2 * dx * f)
+                    x_value = -(h[i, j + 1, k] - h[i, j - 1, k]) / (2 * dy * f)
+                    y_value = (h[i, j, k + 1] - h[i, j, k - 1]) / (2 * dx * f)
                 else:
                     if j == 0:
-                        xvalue = -(h[i, j + 1, k] - h[i, j, k]) / (dy * f)
+                        x_value = -(h[i, j + 1, k] - h[i, j, k]) / (dy * f)
                     elif j == nlat - 1:
-                        xvalue = -(h[i, j, k] - h[i, j - 1, k]) / (dy * f)
+                        x_value = -(h[i, j, k] - h[i, j - 1, k]) / (dy * f)
                     else:
-                        xvalue = -(h[i, j + 1, k] - h[i, j - 1, k]) / (2 * dy * f)
+                        x_value = -(h[i, j + 1, k] - h[i, j - 1, k]) / (2 * dy * f)
 
                     if k == 0:
-                        yvalue = (h[i, j, k + 1] - h[i, j, k]) / (dx * f)
+                        y_value = (h[i, j, k + 1] - h[i, j, k]) / (dx * f)
                     elif k == mlon - 1:
-                        yvalue = (h[i, j, k] - h[i, j, k - 1]) / (dx * f)
+                        y_value = (h[i, j, k] - h[i, j, k - 1]) / (dx * f)
                     else:
-                        yvalue = (h[i, j, k + 1] - h[i, j, k - 1]) / (2 * dx * f)
+                        y_value = (h[i, j, k + 1] - h[i, j, k - 1]) / (2 * dx * f)
 
-                geo_windU[i, j, k] = 9.8 * xvalue
-                geo_windV[i, j, k] = 9.8 * yvalue
+                geo_windU[i, j, k] = 9.8 * x_value
+                geo_windV[i, j, k] = 9.8 * y_value
 
     return geo_windU, geo_windV
 
-def nonageowind(u, v, ug, vg):
+def count_nona_geowind(u, v, ug, vg):
     ua = u - ug
     va = v - vg
     return ua, va
 
-def Divergence(u, v, lat, dy):
+def count_divergence(u, v, lat, dy):
     nlev, nlat, mlon = u.shape
-    div = np.zeros([nlev, nlat, mlon])
+    divergence = np.zeros([nlev, nlat, mlon])
 
     for i in range(nlev):
         for j in range(nlat):
@@ -79,125 +85,289 @@ def Divergence(u, v, lat, dy):
                 dx = dy * np.cos(lat[j] * np.pi / 180)
 
                 if 1 <= j < nlat - 1 and 1 <= k < mlon - 1:
-                    xvalue = (u[i, j, k + 1] - u[i, j, k - 1]) / (2 * dx)
-                    yvalue = (v[i, j + 1, k] - v[i, j - 1, k]) / (2 * dy)
-                    div[i, j, k] = xvalue + yvalue
+                    x_value = (u[i, j, k + 1] - u[i, j, k - 1]) / (2 * dx)
+                    y_value = (v[i, j + 1, k] - v[i, j - 1, k]) / (2 * dy)
+                    divergence[i, j, k] = x_value + y_value
                 else:
                     dx = dy * np.cos(lat[j] * np.pi / 180)
 
                     if k == 0:
-                        xvalue = (u[i, j, k + 1] - u[i, j, k]) / dx
+                        x_value = (u[i, j, k + 1] - u[i, j, k]) / dx
                     elif k == mlon - 1:
-                        xvalue = (u[i, j, k] - u[i, j, k - 1]) / dx
+                        x_value = (u[i, j, k] - u[i, j, k - 1]) / dx
                     else:
-                        xvalue = (u[i, j, k + 1] - u[i, j, k - 1]) / (2 * dx)
+                        x_value = (u[i, j, k + 1] - u[i, j, k - 1]) / (2 * dx)
 
                     if j == 0:
-                        yvalue = (v[i, j + 1, k] - v[i, j, k]) / dy
+                        y_value = (v[i, j + 1, k] - v[i, j, k]) / dy
                     elif j == nlat - 1:
-                        yvalue = (v[i, j, k] - v[i, j - 1, k]) / dy
+                        y_value = (v[i, j, k] - v[i, j - 1, k]) / dy
                     else:
-                        yvalue = (v[i, j + 1, k] - v[i, j - 1, k]) / (2 * dy)
+                        y_value = (v[i, j + 1, k] - v[i, j - 1, k]) / (2 * dy)
 
-                    div[i, j, k] = xvalue + yvalue
+                    divergence[i, j, k] = x_value + y_value
 
-    return div
+    return divergence
 
-def plot_wind_vector(lon, lat, u, v, title):
-    os.makedirs(title[6:], exist_ok=True)
+def plot_wind_vector(lon, lat, u, v):
     wspd = (u ** 2 + v ** 2) ** 0.5
 
     plt.figure(dpi=400)
-    ax = plt.axes(projection=ccrs.PlateCarree())
-    ax.set_extent([80, 170, 20, 65], crs=ccrs.PlateCarree())
+    ax = plt.axes(
+        projection=ccrs.PlateCarree()
+    )
+    ax.set_extent(
+        [80, 170, 20, 65], 
+        crs=ccrs.PlateCarree()
+    )
     ax.add_feature(cfeature.LAND)
     ax.add_feature(cfeature.COASTLINE)
     ax.add_feature(cfeature.BORDERS)
-    ax.set_title(title)
+    ax.set_title("200mb Geostrophic Wind and Height")
 
-    contourf = plt.contourf(lon, lat, wspd, cmap='jet')
-    cbar = plt.colorbar(contourf, location='bottom', orientation='horizontal')
+    contourf = plt.contourf(
+        lon, 
+        lat, 
+        wspd, 
+        cmap='coolwarm'
+    )
+    cbar = plt.colorbar(
+        contourf, 
+        location='bottom', 
+        orientation='horizontal'
+    )
     cbar.set_label("wind speed (m/s)")
 
-    contour = plt.contour(lon, lat, h[0, :, :], levels=np.linspace(10600, 12800, 12), colors='white')
-    plt.clabel(contour, inline=True, fontsize=8, colors='white')
+    contour = plt.contour(
+        lon,
+        lat,
+        h[0, :, :],
+        levels=np.linspace(
+            10600, 
+            12800, 
+            12
+        ),
+        colors='black',
+    )
+    plt.clabel(
+        contour,
+        inline=True,
+        fontsize=8,
+        colors='white',
+    )
 
-    ax.gridlines(draw_labels=[True, "x", "y", "bottom", "left"], linewidth=0.5, color='gray', alpha=0.5, linestyle='--')
+    ax.gridlines(
+        draw_labels=[
+            True, 
+            "x", 
+            "y", 
+            "bottom", 
+            "left"
+        ], 
+        linewidth=0.5, 
+        color='gray', 
+        alpha=0.5, 
+        linestyle='--',
+    )
 
-    plt.quiver(lon[::2], lat[::2], u[::2, ::2], v[::2, ::2], scale_units='xy', scale=12, color='black')
+    plt.quiver(
+        lon[::2], 
+        lat[::2], 
+        u[::2, ::2], 
+        v[::2, ::2], 
+        scale_units='xy', 
+        scale=12, 
+        color='black',
+    )
 
-    plt.savefig(title[6:] + "/" + title + ".png")
+    output_file_name = "200mb_geostrophic_wind_and_height"
+    os.makedirs(
+        output_file_name, 
+        exist_ok=True
+    )
+    plt.savefig(f"{output_file_name}/{output_file_name}.png")
 
     # plt.show()
 
-def plot_wind_divergence(lon, lat, div, u, v, title):
-    os.makedirs(title[6:], exist_ok=True)
+def visualize_wind_divergence(lon, lat, div, u, v):
+    output_file_name = "200mb_geostrophic_wind_height_and_divergence"
+    os.makedirs(
+        output_file_name,
+        exist_ok=True
+    )
 
     plt.figure(dpi=400)
-    ax = plt.axes(projection=ccrs.PlateCarree())
-    ax.set_extent([80, 170, 20, 65], crs=ccrs.PlateCarree())
+    ax = plt.axes(
+        projection=ccrs.PlateCarree()
+    )
+    ax.set_extent(
+        [80, 170, 20, 65], 
+        crs=ccrs.PlateCarree()
+    )
     ax.add_feature(cfeature.LAND)
     ax.add_feature(cfeature.COASTLINE)
     ax.add_feature(cfeature.BORDERS)
-    ax.set_title(title)
+    ax.set_title("200mb Geostrophic Wind, Height and Divergence")
 
-    contourf = plt.contourf(lon, lat, div, cmap='jet')
-    cbar = plt.colorbar(contourf, location='bottom', orientation='horizontal')
-    cbar.ax.tick_params(labelsize=7)
-    cbar.set_label("divergence")
+    contourf = plt.contourf(
+        lon, 
+        lat, 
+        div, 
+        cmap='coolwarm',
+    )
+    cbar = plt.colorbar(
+        contourf, 
+        location='bottom', 
+        orientation='horizontal',
+    )
+    cbar.ax.tick_params(
+        labelsize=7
+    )
+    cbar.set_label("Divergence")
 
-    contour = plt.contour(lon, lat, h[0, :, :], colors='white')
-    plt.clabel(contour, inline=True, fontsize=8, colors='white')
+    contour = plt.contour(
+        lon, 
+        lat, 
+        h[0, :, :], 
+        colors='black'
+    )
+    plt.clabel(
+        contour, 
+        inline=True, 
+        fontsize=8, 
+        colors='white',
+    )
 
-    ax.gridlines(draw_labels=[True, "x", "y", "bottom", "left"], linewidth=0.5, color='gray', alpha=0.5, linestyle='--')
+    ax.gridlines(
+        draw_labels=[
+            True, 
+            "x", 
+            "y", 
+            "bottom", 
+            "left"
+        ], 
+        linewidth=0.5, 
+        color='gray', 
+        alpha=0.5, 
+        linestyle='--',
+    )
 
-    plt.quiver(lon[::2], lat[::2], u[::2, ::2], v[::2, ::2], scale_units='xy', scale=4, color='black')
+    plt.quiver(
+        lon[::2], 
+        lat[::2], 
+        u[::2, ::2], 
+        v[::2, ::2], 
+        scale_units='xy', 
+        scale=4, 
+        color='black'
+    )
 
-    plt.savefig(title[6:] + "/" + title + ".png")
+    plt.savefig(f"{output_file_name}/{output_file_name}.png")
 
     # plt.show()
 
-def plot_data(lon, lat, h, u, v, title):
-    pressure = [200, 500, 1000]
-    os.makedirs(title, exist_ok=True)
+def visualize_wind_height(lon, lat, h, u, v):
+    pressure = [
+        200, 
+        500, 
+        1000
+    ]
+    os.makedirs(
+        "wind_and_height", 
+        exist_ok=True,
+    )
     wspd = (u ** 2 + v ** 2) ** 0.5
 
     for i in range(len(pressure)):
         wind_level = 20 - 5 * i
         plt.figure(dpi=400)
-        ax = plt.axes(projection=ccrs.PlateCarree())
-        ax.set_extent([80, 170, 20, 65], crs=ccrs.PlateCarree())
+        ax = plt.axes(
+            projection=ccrs.PlateCarree()
+        )
+        ax.set_extent(
+            [80, 170, 20, 65], 
+            crs=ccrs.PlateCarree()
+        )
         ax.add_feature(cfeature.LAND)
         ax.add_feature(cfeature.COASTLINE)
         ax.add_feature(cfeature.BORDERS)
-        titles = str(pressure[i]) + title
-        ax.set_title(titles)
+        title = f"{pressure[i]}mb Wind and Height"
+        ax.set_title(title)
+        file_name = f"{pressure[i]}mb_wind_and_height.png"
 
-        contourf = plt.contourf(lon, lat, wspd[i, :, :], cmap='jet')
-        cbar = plt.colorbar(contourf, location='bottom', orientation='horizontal')
+        contourf = plt.contourf(
+            lon, 
+            lat, 
+            wspd[i, :, :], 
+            cmap='coolwarm',
+        )
+        cbar = plt.colorbar(
+            contourf, 
+            location='bottom', 
+            orientation='horizontal',
+        )
         cbar.set_label("wind speed (m/s)")
 
-        contour = plt.contour(lon, lat, h[i, :, :], colors='white')
-        plt.clabel(contour, inline=True, fontsize=8, colors='white')
+        contour = plt.contour(
+            lon, 
+            lat, 
+            h[i, :, :], 
+            colors='black'
+        )
+        plt.clabel(
+            contour, 
+            inline=True, 
+            fontsize=8, 
+            colors='white',
+        )
 
-        ax.gridlines(draw_labels=[True, "x", "y", "bottom", "left"], linewidth=0.5, color='gray', alpha=0.5, linestyle='--')
+        ax.gridlines(
+            draw_labels=[
+                True, 
+                "x", 
+                "y", 
+                "bottom", 
+                "left"
+            ], 
+            linewidth=0.5, 
+            color='gray', 
+            alpha=0.5, 
+            linestyle='--'
+        )
 
-        q = plt.quiver(lon[::2], lat[::2], u[i, ::2, ::2], v[i, ::2, ::2], scale_units='xy', scale=10 - 3 * i, color='black')
-        qk = plt.quiverkey(q, 0.8, 1.05, wind_level, str(wind_level) + ' m/s', labelpos='E', coordinates='axes',
-                          fontproperties={'size': 8})
+        q = plt.quiver(
+            lon[::2], 
+            lat[::2], 
+            u[i, ::2, ::2], 
+            v[i, ::2, ::2], 
+            scale_units='xy', 
+            scale=10 - 3 * i, 
+            color='black',
+        )
+        plt.quiverkey(
+            q, 
+            0.8, 
+            1.05, 
+            wind_level, 
+            str(wind_level) + ' m/s', 
+            labelpos='E', 
+            coordinates='axes',
+            fontproperties={'size': 8}
+        )
 
-        plt.savefig(title + "/" + titles + ".png")
+        plt.savefig(f"wind_and_height/{file_name}.png")
 
         # plt.show()
 
 if __name__ == "__main__":
-    dy = 6378000 * np.pi/180
+    dy = 6378000 * np.pi / 180
     filename = './data/fnldata.dat'
     data = load_data(filename)
     lon, lat, h, u, v = configure_parameters(data)
-    ug, vg = geowind(h, lat, lon)
-    plot_wind_vector(lon, lat, ug[0, :, :], vg[0, :, :], "200mb geostrophic wind and height")
-    div = Divergence(u, v, lat, dy)
-    ua, va = nonageowind(u, v, ug, vg)
-    plot_wind_divergence(lon, lat, div[0, :, :], ua[0, :, :], va[0, :, :], "200mb geostrophic wind, height and divergence")
-    plot_data(lon, lat, h, u, v, "wind and height")
+    ug, vg = count_geowind(h, lat, lon)
+    plot_wind_vector(lon, lat, ug[0, :, :], vg[0, :, :])
+    div = count_divergence(u, v, lat, dy)
+    ua, va = count_nona_geowind(u, v, ug, vg)
+    visualize_wind_divergence(lon, lat, div[0, :, :], ua[0, :, :], va[0, :, :])
+    visualize_wind_height(lon, lat, h, u, v)
+
